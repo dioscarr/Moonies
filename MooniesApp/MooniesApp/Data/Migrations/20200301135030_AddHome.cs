09379a2d46be4e53a4e86acfad8f46277050699e
Dioscar Rodriguez <dioscarr@gmail.com>
2020-03-01 10:01:40 -0500

    added Home entity and updated database

---
+++ MooniesApp/MooniesApp/Data/Migrations/20200301135030_AddHome.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MooniesApp.Data.Migrations
+{
+    public partial class AddHome : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Homes",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Homes", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Homes");
+        }
+    }
+}
